a2b516e63a2e80e38e21868041fd7aa8fb8b45b7
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-02-08 18:00:48 +0000

    test status codes

--- test/server/main.spec.js
+++ test/server/main.spec.js
@@ -9,27 +9,42 @@ describe('Middelware', () => {
 	before(() => {
 		app = express();
 		app.use(jsonpMiddleware);
-		app.get('/', (req, res) => {
+		app.get('/good', (req, res) => {
 			res.jsonp('a response');
+		});
+		app.get('/bad', (req, res) => {
+			res.status(404).jsonp('a response');
 		});
 	});
 
 	it('should return a 200 with a status in the response', () => {
 		return request(app)
-			.get('/?callback=foo')
+			.get('/good?callback=foo')
 			.expect(200, /"status":200/);
+	});
+
+	it('should return a 200 with the set status code in the reponse', () => {
+		return request(app)
+			.get('/bad?callback=foo')
+			.expect(200, /"status":404/);
 	});
 
 	it('should return a body in the response', () => {
 		return request(app)
-			.get('/?callback=foo')
+			.get('/good?callback=foo')
 			.expect(/"body":"a response"/);
 	});
 
 	it('should return json if no callback', () => {
 		return request(app)
-			.get('/')
+			.get('/good')
 			.expect(200, '"a response"');
+	});
+
+	it('should return set status code', () => {
+		return request(app)
+			.get('/bad')
+			.expect(404);
 	});
 
 });
