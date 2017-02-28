5052cf082c38eb89f3f82cb9fad432b293a5267b
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-02-28 13:12:31 +0000

    Use webpack

--- test/client/karma.conf.js
+++ test/client/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['browserify', 'mocha', 'chai-as-promised', 'chai'],
+		frameworks: ['mocha', 'chai-as-promised', 'chai'],
 
 
 		// list of files / patterns to load in the browser
@@ -27,15 +27,7 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'**/*.js': [ 'browserify' ]
-		},
-
-
-		browserify: {
-			transform: [
-				['babelify', { presets: [ 'es2015' ] } ],
-				'debowerify'
-			]
+			'**/*.js': [ 'webpack' ]
 		},
 
 		// test results reporter to use
