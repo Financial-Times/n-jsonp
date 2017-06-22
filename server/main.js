cd27b963c4130f6a5b78166ba6b8f61fb68725ed
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-06-22 12:24:16 +0100

    Backward's compatiable export

--- server/main.js
+++ server/main.js
@@ -1,19 +1,21 @@
-module.exports = (req, res, next) => {
-	const existingJsonp = res.jsonp;
-	const newJsonp = body => {
-		// if it's jsonp...
-		const callbackName = res.app.get('jsonp callback name');
-		if (req.query[callbackName]) {
-			const status = res.statusCode;
-			res.status(200);
-			body = {
-				status,
-				body
-			};
-		}
-		existingJsonp.call(res, body);
-	};
+module.exports = {
+	default: (req, res, next) => {
+		const existingJsonp = res.jsonp;
+		const newJsonp = body => {
+			// if it's jsonp...
+			const callbackName = res.app.get('jsonp callback name');
+			if (req.query[callbackName]) {
+				const status = res.statusCode;
+				res.status(200);
+				body = {
+					status,
+					body
+				};
+			}
+			existingJsonp.call(res, body);
+		};
 
-	res.jsonp = newJsonp;
-	next();
+		res.jsonp = newJsonp;
+		next();
+	}
 };
